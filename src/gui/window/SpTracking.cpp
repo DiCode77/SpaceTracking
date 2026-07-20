b7dab16f2d36c3cb1728c9ee4f7ac8f49653d16d
DiCode77 <dimazamshaq@icloud.com>
2026-07-20 20:45:34 +0300

    Code optimization has been performed, and the functionality for determining the location of objects has been improved.

--- src/gui/window/SpTracking.cpp
+++ src/gui/window/SpTracking.cpp
@@ -7,42 +7,46 @@
 
 #include "SpTracking.hpp"
 
-SpTracking::SpTracking(const wxString title, const wxPoint point, const wxSize size) : wxFrame(nullptr, wxID_ANY, title, point, size){
-    fsize size_map = {
-        (double)this->GetClientSize().GetX() - 400,
-        (double)this->GetClientSize().GetY() - 200
-    };
-    
-    fpoint point_map = {
-        ((double)this->GetClientSize().GetX() - size_map.x) /2,
-        ((double)this->GetClientSize().GetY() - size_map.y) /2
-    };
-    
-    this->m_panel = new wxPanel(this, wxID_ANY, wxPoint(point_map.x, point_map.y), wxSize(size_map.x, size_map.y));
-    this->m_static_box_map = new wxStaticBox(this->m_panel, wxID_ANY, wxT(""), wxPoint(0, 0), wxSize(size_map.x, size_map.y));
-    this->m_map = new MapKit(this->m_panel->GetHandle(), fpoint(10, 10), fsize(size_map.x -20, size_map.y +10), fscale::fnone);
+SpTracking::SpTracking(const wxString title, const wxPoint point, const wxSize size) : wxFrame(nullptr, wxID_ANY, title, point, size), m_size_main_frame(size){
+   
+    auto &&prop1 = this->GetNewProperty(this->GetClientSize(), size, wxSize(700, 500), wxPoint(2, 2));
+    prop1.first.y -= 20;
+    this->m_panel_map = new wxPanel(this, wxID_ANY, prop1.first, prop1.second);
+    this->m_panel_map->Bind(wxEVT_SIZE, &SpTracking::OnSizePanelMap, this);
+    this->m_static_box_map = new wxStaticBox(this->m_panel_map, wxID_ANY, wxT(""), wxPoint(0, 0), prop1.second);
+    this->m_map = new MapKit(this->m_panel_map->GetHandle(), fpoint(10, 10), fsize(prop1.second.x -20, prop1.second.y +10), fscale::fnone);
     
     this->Bind(wxEVT_SIZE, &SpTracking::OnSizeMainFrame, this);
 }
 
+std::pair<wxPoint, wxSize> SpTracking::GetNewProperty(const wxSize &p_updated, const wxSize &p_oem, const wxSize &p_user_size, const wxPoint &p_point, bool s_point){
+    wxSize new_updated_size = (p_updated - p_oem) + p_user_size;
+    wxSize new_point        =  p_updated - new_updated_size;
+    
+    if (s_point){
+        new_point.x /= p_point.x;
+        new_point.y /= p_point.y;
+    }else{
+        new_point.x = p_point.x;
+        new_point.y = p_point.y;
+    }
+    
+    return std::make_pair(wxPoint(new_point.x, new_point.y), new_updated_size);
+}
+
 void SpTracking::OnSizeMainFrame(wxSizeEvent &event){
-    wxSize size_main_frame_naw = event.GetSize();
-    fsize size_map = {
-        (double)size_main_frame_naw.GetX() - 400,
-        (double)size_main_frame_naw.GetY() - 200
-    };
+    auto &&prop1 = this->GetNewProperty(event.GetSize(), this->m_size_main_frame, wxSize(700, 500), wxPoint(2, 2));
+    prop1.first.y -= 20;
+    this->m_panel_map->SetPosition(prop1.first);
+    this->m_panel_map->SetSize(prop1.second);
     
-    fpoint point_map = {
-        ((double)size_main_frame_naw.GetX() - size_map.x) /2,
-        ((double)size_main_frame_naw.GetY() - size_map.y) /2
-    };
-    
-    this->m_panel->SetPosition(wxPoint(point_map.x, point_map.y));
-    this->m_panel->SetSize(size_map.x, size_map.y);
-    
+    std::cout << prop1.second.x << std::endl;
+}
+
+void SpTracking::OnSizePanelMap(wxSizeEvent &event){
     this->m_static_box_map->SetPosition(wxPoint(0, 0));
-    this->m_static_box_map->SetSize(this->m_panel->GetSize());
+    this->m_static_box_map->SetSize(event.GetSize());
     
     this->m_map->setPoint(fpoint(10, 10));
-    this->m_map->setSize(fsize(size_map.x -20, size_map.y -20));
+    this->m_map->setSize(fsize(event.GetSize().x -20, event.GetSize().y -20));
 }
--- src/gui/window/SpTracking.hpp
+++ src/gui/window/SpTracking.hpp
@@ -13,14 +13,18 @@
 #include <MapKit.hpp>
 
 class SpTracking : public wxFrame{
+    const wxSize m_size_main_frame;
 private:
-    wxPanel     *m_panel;
+    wxPanel     *m_panel_map;
     wxStaticBox *m_static_box_map;
     MapKit      *m_map;
 public:
     SpTracking(const wxString title, const wxPoint point, const wxSize size);
 private:
+    // 1 - updates size, 2 - oem size, 3 - user size, 4 - point size, set user point
+    std::pair<wxPoint, wxSize> GetNewProperty(const wxSize&, const wxSize&, const wxSize&, const wxPoint&, bool = true);
     void OnSizeMainFrame(wxSizeEvent&);
+    void OnSizePanelMap(wxSizeEvent&);
 };
 
 #endif /* SpTracking_hpp */
